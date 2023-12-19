cc54c26bbe6db7b90d2737ef77002f780fdf18ff
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2023-12-19 16:45:13 +0530

    cmd line args & scanner

--- main.go
+++ main.go
@@ -1,38 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
-// ********************************Example 2********************************
+// ********************************Example 4 🏂********************************
 
 func main() {
-	var sum float64
-	var n int
 
-	for {
-		var val float64
+	// str := "Ashiq.Hussain.Kumar"
+	// arr := strings.Split(str, ".")
+	// str = strings.Join(arr, "#")
+	// fmt.Println(str, len(arr))
 
-		_, err := fmt.Fscanln(os.Stdin, &val)
-
-		if err != nil {
-			break
-		}
-
-		sum += val
-		n++
-	}
-
-	if n == 0 {
-		fmt.Fprintln(os.Stderr, "no values")
+	if len(os.Args) < 3 {
+		fmt.Fprint(os.Stderr, "not enough args")
 		os.Exit(-1)
 	}
 
-	fmt.Println("The average is:", sum/float64(n))
+	old, new := os.Args[1], os.Args[2]
+	scan := bufio.NewScanner(os.Stdin)
+
+	for scan.Scan() {
+		s := strings.Split(scan.Text(), old)
+		t := strings.Join(s, new)
+
+		fmt.Println(t)
+	}
 }
 
-// ********************************Example 1********************************
+// ********************************Example 3 🏂********************************
+
+// func main() {
+// 	elite := "ělite"
+// 	fmt.Printf("%T %[1]v %d\n", elite, len(elite))
+// 	fmt.Printf("%T %[1]v\n", []rune(elite))
+// 	fmt.Printf("%T %[1]v\n", []byte(elite))
+// }
+
+// ********************************Example 2 🏂********************************
+
+// func main() {
+// 	var sum float64
+// 	var n int
+
+// 	for {
+// 		var val float64
+
+// 		_, err := fmt.Fscanln(os.Stdin, &val)
+
+// 		if err != nil {
+// 			break
+// 		}
+
+// 		sum += val
+// 		n++
+// 	}
+
+// 	if n == 0 {
+// 		fmt.Fprintln(os.Stderr, "no values")
+// 		os.Exit(-1)
+// 	}
+
+// 	fmt.Println("The average is:", sum/float64(n))
+// }
+
+// ********************************Example 1 🏂********************************
 
 // import (
 // 	"fmt"
